20eae3184fe2c0d96e1f68d7aec0e2782162a0a0
Juan <jlizarraga@unomaha.edu>
2021-03-06 12:35:33 -0600

    Update models.py

--- chineseSpicyFlavor/models.py
+++ chineseSpicyFlavor/models.py
@@ -103,3 +103,20 @@ class Order(models.Model):
     def __str__(self):
         """String representation for an order object"""
         return f'{self.id}'
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,
+                              related_name='items',
+                              on_delete=models.CASCADE)
+    product = models.ForeignKey(Item,
+                                related_name='order_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.order.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
