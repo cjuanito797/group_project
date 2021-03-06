73b90b07aef31b58b664971cf3ff7ec67b76631f
Juan <jlizarraga@unomaha.edu>
2021-03-06 12:35:23 -0600

    Update admin.py

--- chineseSpicyFlavor/admin.py
+++ chineseSpicyFlavor/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Item, Customer, Order, Category, Profile
+from .models import Item, Customer, Order, Category, Profile, OrderItem
 
 # Register your models here.
 admin.site.register(Item)
 admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(Category)
-
+admin.site.register(OrderItem)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
