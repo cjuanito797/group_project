7f17f8dfd0a2f1144b833a5ad791df2077056635
Juan <jlizarraga@unomaha.edu>
2021-03-24 18:20:04 -0600

    Update urls.py

--- group_project/urls.py
+++ group_project/urls.py
@@ -21,6 +21,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chineseSpicyFlavor.urls')),
+    path('cart/', include('cart.urls', namespace='cart')),
+
 ]
 
 if settings.DEBUG:
