e2166120f76f8a4de5b6bbdf4e11540d5b40aa25
Juan <jlizarraga@unomaha.edu>
2021-04-05 18:38:10 -0500

    Update models.py

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class account_data(models.Model):
+    username = models.CharField(max_length=30)
+    key = models.CharField(max_length=30)
+    value = models.CharField(max_length=30, null=True, blank=False)
+
+    class Meta:
+        unique_together=("username", "key")
