dc864582c4285913ec39408e646eadbfe4acc80f
Juan <jlizarraga@unomaha.edu>
2021-04-13 16:56:27 -0500

    Update models.py

--- orders/models.py
+++ orders/models.py
@@ -53,6 +53,14 @@ class OrderItem(models.Model):
 
 
 class GuestOrder(models.Model):
+    id = models.CharField(
+        primary_key=True,
+        max_length=6,
+        blank=True,
+        editable=False,
+        unique=False,
+        default=create_ref_number()
+    )
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField()
@@ -72,3 +80,20 @@ class GuestOrder(models.Model):
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+
+
+class GuestOrderItem(models.Model):
+    guest_order = models.ForeignKey(GuestOrder,
+                                    related_name='items',
+                                    on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,
+                                related_name='guestorder_items',
+                                on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return '{}'.format(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
