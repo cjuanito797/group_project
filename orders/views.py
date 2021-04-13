5f633808ae66020bca0a3d5aca5af9c715e76285
Juan <jlizarraga@unomaha.edu>
2021-04-13 16:56:33 -0500

    Update views.py

--- orders/views.py
+++ orders/views.py
@@ -1,16 +1,17 @@
 from django.urls import reverse
 from django.shortcuts import render, redirect
-from .models import OrderItem
+from .models import OrderItem, GuestOrderItem
 from cart.cart import Cart
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
-from .models import Order
+from .models import Order, GuestOrder
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from chineseSpicyFlavor.models import Profile, Address
 from chineseSpicyFlavor.views import display_addresses
 from .forms import OrderCreateForm
+
 
 # import weasyprint
 
@@ -48,22 +49,20 @@ def order_create(request):
         if request.method == 'POST':
             form = OrderCreateForm(request.POST)
             if form.is_valid():
-                order = form.save()
+                guest_order = form.save()
                 for item in cart:
-                    OrderItem.objects.create(order=order,
-                                             product=item['product'],
-                                             price=item['price'],
-                                             quantity=item['quantity'])
+                    GuestOrderItem.objects.create(guest_order=guest_order,
+                                                  product=item['product'],
+                                                  price=item['price'],
+                                                  quantity=item['quantity'])
                 # clear the cart
                 cart.clear()
                 # set the order in the session
-                request.session['order_id'] = order.id
                 # redirect for payment
-                return redirect(reverse('payment:process'))
+                return render(request, 'orders/order/created.html')
 
         else:
             form = OrderCreateForm()
         return render(request,
                       'orders/order/create.html',
                       {'cart': cart, 'form': form})
-
