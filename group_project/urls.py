d32cc959e2522bd59d4510edbc2efd320cd8946b
Juan <jlizarraga@unomaha.edu>
2021-04-27 14:03:53 -0500

    Update urls.py

--- group_project/urls.py
+++ group_project/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('', include('chineseSpicyFlavor.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
     path('orders/', include('orders.urls', namespace='orders')),
+    path('payment/', include('payment.urls', namespace='payment')),
 
 ]
 
