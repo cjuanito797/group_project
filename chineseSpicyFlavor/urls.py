e85fa9f86b314fdd82ea4971e8d96eacf2e81a44
sweta-123-gif <68089689+sweta-123-gif@users.noreply.github.com>
2021-04-29 00:37:04 -0500

    updated

--- chineseSpicyFlavor/urls.py
+++ chineseSpicyFlavor/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done.html'),
     path('foods/', views.product_list, name='product_list'),
     path('foods/<slug:category_slug>/', views.product_list,
          name='product_list_by_category'),
