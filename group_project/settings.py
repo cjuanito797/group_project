83bec1c4b5792f4b256aec996a95a04782815af6
Juan <jlizarraga@unomaha.edu>
2021-05-04 23:18:16 -0500

    Update settings.py

--- group_project/settings.py
+++ group_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@x$d3zb^^^l-$_rf*x8-#i=%hl*ig6=*jw7#pe3cr1u-ra)q28'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
